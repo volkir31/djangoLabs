a7b210d7efb662f281595fa1f664fc8941d6634d
Egor <egor.mixajlichenko@gmail.com>
2023-12-02 23:03:54 +0300

    init

--- VKParser/settings.py
+++ VKParser/settings.py
@@ -74,12 +74,8 @@ WSGI_APPLICATION = 'VKParser.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'study',
-        'USER': 'userdb',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
