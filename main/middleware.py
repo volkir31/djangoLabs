a6e5517630cef7700b7f665d5ad232c2b5c97867
Egor <egor.mixajlichenko@gmail.com>
2023-12-16 21:35:38 +0300

    init

--- main/middleware.py
+++ main/middleware.py
@@ -1,9 +1,10 @@
 import datetime
+import os
 
 
 def timing(get_response):
     def middleware(request):
-        with open('logs/logs.log', 'a') as f:
+        with open(f"{os.getenv('LOG_FILE')}", 'a') as f:
             f.write(f"{datetime.datetime.now()} {request.META.get('REMOTE_ADDR')}\n")
         response = get_response(request)
         return response
